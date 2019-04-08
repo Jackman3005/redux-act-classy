b75bbfdd4456885d3ff70e6832ff19b5fd7d88a6
Jack Coy <jackman3000@gmail.com>
2019-04-08 12:08:24 +1000

    Update tests to handle non-default exports at top level

--- test/redux-easy-actions.test.ts
+++ test/redux-easy-actions.test.ts
@@ -1,10 +1,10 @@
-import easyActions from '../src/redux-easy-actions'
+import * as easyActions from '../src/redux-easy-actions'
 
 describe('redux-easy-actions', () => {
-    it('exports library features', () => {
-        expect(easyActions).toBeDefined()
+  it('exports library features', () => {
+    expect(easyActions).toBeDefined()
 
-        expect(easyActions.EasyAction).toBeDefined()
-        expect(easyActions.easyActionsMiddleware).toBeDefined()
-    })
+    expect(easyActions.EasyAction).toBeDefined()
+    expect(easyActions.easyActionsMiddleware).toBeDefined()
+  })
 })
