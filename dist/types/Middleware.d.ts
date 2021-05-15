10c23713eb4f1cbea2fcafbb9c03809f40850cae
Jack Coy <jackman3000@gmail.com>
2021-05-15 19:03:50 +1000

    chore(package.json): bump version of semantic-relase
    also add dist folder to .gitignore, all those files can be found on npmjs.org

--- dist/types/Middleware.d.ts
+++
@@ -1,5 +0,0 @@
-import { Middleware } from 'redux';
-export interface MiddlewareConfig {
-    dispatchLifecycleActions: boolean;
-}
-export declare const buildAClassyMiddleware: (config?: Partial<MiddlewareConfig> | undefined) => Middleware<{}, any, import("redux").Dispatch<import("redux").AnyAction>>;
--- dist/types/redux-act-classy.d.ts
+++
@@ -1,4 +0,0 @@
-import 'core-js/features/promise';
-export { Classy } from './ClassyAction';
-export { buildAClassyMiddleware } from './Middleware';
-export { afterComplete, afterError, afterSuccess, beforeStart, isAction } from './Helpers';
